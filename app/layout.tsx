a823f791a0e35cb5426bd8b69d22dbf41126d9ee
Tianyuan Bi <129766707+tbi880@users.noreply.github.com>
2023-06-23 06:15:25 +1200

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "VIT-AI助理",
+  description: "创建属于不同角色的对话式AI助理",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "VIT-AI助理",
     statusBarStyle: "default",
   },
 };
